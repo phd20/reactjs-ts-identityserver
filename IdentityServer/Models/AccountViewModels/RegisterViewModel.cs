45fea093b0686539b449fac50416bb39e8e9830a
PhD20 <dmprofessor227@gmail.com>
2020-03-23 20:50:42 -0500

    Add Username to workflow

--- IdentityServer/Models/AccountViewModels/RegisterViewModel.cs
+++ IdentityServer/Models/AccountViewModels/RegisterViewModel.cs
@@ -9,6 +9,11 @@ namespace IdentityServerWithAspNetIdentity.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Username")]
+        [StringLength(24, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+        public string UserName { get; set;}
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
